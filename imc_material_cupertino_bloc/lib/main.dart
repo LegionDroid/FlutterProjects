5608744496f900b3d526d59588e916d3851e2f07
LegionDroid <brunolegion@hotmail.com>
2020-01-13 00:02:47 -0300

    add project

---
+++ imc_material_cupertino_bloc/lib/main.dart
@@ -0,0 +1,10 @@
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
+import 'package:imc_material_cupertino_bloc/ui/android/material-app.dart';
+import 'package:imc_material_cupertino_bloc/ui/ios/cupertino-app.dart';
+
+// void main() => runApp(MyMaterialApp());
+// void main() => runApp(MyCupertinoApp());
+
+void main() =>
+  Platform.isIOS ? runApp(MyCupertinoApp()) : runApp(MyMaterialApp());
